d7fad3e55be40d6a08f5f5e6ef44cb0e44784a52
WengX <1027382710@qq.com>
2025-05-06 20:59:50 +0800

    fix: 修复 UnoCSS 规则配置中样式嵌套的问题

--- uno.config.js
+++ uno.config.js
@@ -1,19 +1,18 @@
 // import presetWind4 from '@unocss/preset-wind4'
 import presetWind3 from '@unocss/preset-wind3'
-import { defineConfig } from 'unocss'
+import { defineConfig, toEscapedSelector as e } from 'unocss'
 
 export default defineConfig({
   presets: [presetWind3()],
   rules: [
     // 隐藏滚动条但保留滚动功能
     [
-      'hide-scrollbar',
-      {
-        '-ms-overflow-style': 'none' /* IE/Edge */,
-        'scrollbar-width': 'none' /* Firefox */,
-        '&::-webkit-scrollbar': {
-          /* WebKit */ display: 'none',
-        },
+      /^hide-scrollbar$/,
+      (_, record) => {
+        const { rawSelector } = record
+        const selector = e(rawSelector)
+        return `${selector}{-ms-overflow-style: none;scrollbar-width: none;}
+${selector}::-webkit-scrollbar{display: none;}`
       },
     ],
   ],
