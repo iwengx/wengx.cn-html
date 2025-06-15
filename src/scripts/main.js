b4bf2b3bc60801fc2cdd4d6707603166f2c4fccd
WengX <1027382710@qq.com>
2025-06-15 21:34:56 +0800

    feat: 卡片旋转的方式展示赞助列表页面

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
+/** 监听滚动事件，当滚动时 logo 旋转 90 度 */
 const wrapperEl = document.getElementById('wengxcn-wrapper')
 const logoEl = document.getElementById('wengxcn-logo')
-
-// 监听滚动事件，当滚动时 logo 旋转 90 度
 wrapperEl.addEventListener('scroll', (e) => {
   const { scrollTop } = e.target
   if (scrollTop > 0) {
@@ -11,10 +10,36 @@ wrapperEl.addEventListener('scroll', (e) => {
   }
 })
 
-// 软件版本
+/** 软件下载配置 */
 const llhVersion = '2.5.6'
-// 下载链接
 const downloadLink = `https://slink.ltd/https://github.com/iwengx/Minecraft-Lil-liver-helper/releases/download/${llhVersion}/minecraft-lil-liver-helper-v${llhVersion}.exe`
-// 给下载按钮添加下载链接
 const downloadEl = document.getElementById('wengxcn-download')
 downloadEl.setAttribute('href', downloadLink)
+
+/** 卡片翻转 */
+const faceCreeper = document.querySelector('#face-creeper')
+const backCreeper = document.querySelector('#back-creeper')
+const homeMain = document.querySelector('#home-main')
+const sponsorMain = document.querySelector('#sponsor-main')
+
+const sponsorIframe = document.createElement('iframe')
+sponsorIframe.src = './support.html'
+sponsorIframe.className = 'w-full h-full'
+sponsorIframe.setAttribute('frameborder', '0')
+
+faceCreeper.addEventListener('click', () => {
+  homeMain.style.transform = 'rotateY(-180deg)'
+  homeMain.style.zIndex = '0'
+  sponsorMain.style.transform = 'rotateY(0)'
+  sponsorMain.style.zIndex = '10'
+  if (!sponsorMain.contains(sponsorIframe)) {
+    sponsorMain.appendChild(sponsorIframe)
+  }
+})
+
+backCreeper.addEventListener('click', () => {
+  homeMain.style.transform = ''
+  homeMain.style.zIndex = ''
+  sponsorMain.style.transform = ''
+  sponsorMain.style.zIndex = ''
+})
--- uno.config.js
+++ uno.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
     // 底部的链接按钮
     'btn-link':
       'flex items-center mx-1 rounded p-[5px] transition-[box-shadow,background-color] duration-150 ease-in-out hover:bg-[#f7d51d] hover:shadow-[rgba(100,100,111,0.2)_0px_7px_29px_0px]',
+
+    // 首页卡片容器
+    'main-container':
+      'w-full h-full md:aspect-video md:max-w-[1680px] md:max-h-[1050px] md:rounded-xl bg-white overflow-hidden shadow-[rgba(0,0,0,0.45)_0px_25px_20px_-20px] backface-hidden transition-transform transition-duration-800',
   },
   rules: [
     // 隐藏滚动条但保留滚动功能
@@ -21,15 +25,6 @@ ${selector}::-webkit-scrollbar{display: none;}`
       },
     ],
 
-    [
-      /^border-pixel-(\w+)$/,
-      ([, c]) => ({
-        'border-style': 'solid',
-        'border-image': `linear-gradient(45deg, var(--un-color-${c}-600) 25%, transparent 25%, transparent 75%, var(--un-color-${c}-600) 75%), linear-gradient(45deg, var(--un-color-${c}-600) 25%, transparent 25%, transparent 75%, var(--un-color-${c}-600) 75%)`,
-        'border-image-size': '4px 4px',
-        'border-image-repeat': 'repeat',
-      }),
-    ],
     ['text-minecraft', { 'text-shadow': '2px 2px 0px rgba(0,0,0,0.8)' }],
     ['text-minecraft-sm', { 'text-shadow': '1px 1px 0px rgba(0,0,0,0.8)' }],
     ['shadow-minecraft', { 'box-shadow': 'inset -4px -4px 0px rgba(0,0,0,0.3), inset 4px 4px 0px rgba(255,255,255,0.1)' }],
